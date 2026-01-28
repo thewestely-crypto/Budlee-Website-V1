77b2422b2ef7da27a3b7a290901ba26d099e81f1
Saleej Kuruniyan <saleejkuruniyan@gmail.com>
2026-01-28 11:11:21 +0300

    Mixpanel bugfix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-// Track initial page view
-mixpanel.track('Page Viewed', {
-  page: window.location.pathname
-});
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
@@ -10,6 +7,11 @@ import mixpanel from 'mixpanel-browser';
 
 // Initialize MixPanel (replace 'YOUR_PROJECT_TOKEN' with your actual token)
 mixpanel.init('0f5cade3a076f642604937266b00fc27');
+
+// Track initial page view (after init)
+mixpanel.track('Page Viewed', {
+  page: window.location.pathname
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
